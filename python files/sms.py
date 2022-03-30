e94d1822fc8505bce706deec94c3fbbc30ccba29
Miriam Harb <harbm17@students.ecu.edu>
2022-03-30 17:14:53 -0400

    added SMS python file

---
+++ python files/sms.py
@@ -0,0 +1,9 @@
+from twilio.rest import Client 
+ 
+account_sid = 'AC222d9accf4275e0a382642ca769af586' 
+auth_token = '[3e478e164fad246b1b7144e4dbb0eb75]' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create(to='+19199854106')
+ 
+print(message.sid)
