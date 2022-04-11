cbea27352aab24d3651c3cf69d748d6841ed1b12
Brandon Woods <woodsb18@students.ecu.edu>
2022-04-10 22:24:45 -0400

    Update PersonalInformation.py

--- python files/PersonalInformation.py
+++ python files/PersonalInformation.py
@@ -4,37 +4,42 @@ This file is for the personal information objects
 These objects are made from the information that people will enter
 """
 
-class PersonalInfo(object):
 
+class PersonalInfo(object):
     """
     All personal info objects must contain a name, number, and party size associated with the object.
     These will be given by the submitted data
     """
-    def __init__(self, name, number, partySize):
+
+    def __init__(self, name, email, partySize):
         self.name = name
-        self.number = number
+        self.email = email
         self.partySize = partySize
 
     """
     Simple get function to return a name if needed
     """
+
     def getName(self):
         return self.name
 
     """
     Simple get function to return a number if needed
     """
-    def getNumber(self):
-        return self.number
+
+    def getEmail(self):
+        return self.email
 
     """
     Simple get function to return party size if needed
     """
+
     def getPartySize(self):
         return self.partySize
 
     """
     Display the information of the object to the console
     """
+
     def printInfo(self):
-        print("Name: {}\nNumber: {}\nParty Size {}".format(self.name, self.number, self.partySize))
+        print("Name: {}\nEmail: {}\nParty Size {}".format(self.name, self.email, self.partySize))
